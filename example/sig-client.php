5b5a24d554dbd964827642d5c0c1d0f15b685d0b
tsyk goto <ngyuki.ts@gmail.com>
2013-02-04 12:00:00 +0900

    fix example autoload

--- example/sig-client.php
+++ example/sig-client.php
@@ -1,7 +1,13 @@
 <?php
 namespace ngyuki\SimpleWorker;
 
-require __DIR__ . '/../vendor/autoload.php';
+foreach (array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php') as $fn)
+{
+    if (file_exists($fn))
+    {
+        require $fn;
+    }
+}
 
 $client = new SimpleWorkerClient();
 $client->send();
